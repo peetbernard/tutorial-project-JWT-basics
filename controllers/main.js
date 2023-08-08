5b3dbf657f9314d4fb24da819cbddcf21d528ce7
peetbernard <inline21@gmail.com>
2023-08-08 08:22:04 +0200

    first push to github, not finalized

---
+++ controllers/main.js
@@ -0,0 +1,18 @@
+
+
+
+const login = async (req,res) => {
+    const {username,password} = req.body
+
+
+
+    
+    console.log(username,password)
+    res.send('Fake Login/Register/Signup')
+}
+
+const dashboard = async (req,res) => {
+    const luckyNumber = Math.floor(Math.random()*100)
+    res.status(200).json({msg:`Hello, John Doe`, secret: `Here is your authorized data, your lucky number is ${luckyNumber}`})}
+
+    module.exports = {login,dashboard}
